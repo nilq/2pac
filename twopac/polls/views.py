42fac25e141da9fe3cdb7155d2473a0e8a62f426
niels <i.am@nilq.dk>
2018-07-17 21:17:16 +0200

    [chapter 4] Add voting system using POST

--- twopac/polls/views.py
+++ twopac/polls/views.py
@@ -1,26 +1,44 @@
-from django.shortcuts import render, HttpResponse
+from django.http      import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.template  import loader
+from django.urls      import reverse
+from django.db.models import F
 
 from .models import Question
 
 
 def index(request):
   latest_top = Question.objects.order_by('-publish_date')[:5]
-  
+
   template = loader.get_template('polls/index.html')
 
   context = {
     'latest_top': latest_top
   }
 
-  return HttpResponse(template.render(context, request))
+  return render(request, 'polls/index.html', context)
 
-def detail(question, question_id):
-  return HttpResponse("You're looking at question %s" % question_id)
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+
+  return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-  response = "You're looking at the results of question %s"
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s" % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question':      question,
+      'error_message': "Please do feel free to select a choice."
+    })
+  else:
+    selected_choice.votes = F('votes') + 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))

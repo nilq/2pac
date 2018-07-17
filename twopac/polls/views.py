4842d790ffba91a5744e69d8563b4fdbf119bd5c
niels <i.am@nilq.dk>
2018-07-17 20:47:04 +0200

    [chapter 3] Add template, views etc

--- twopac/polls/views.py
+++ twopac/polls/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, HttpResponse
+from django.template  import loader
 
-# Create your views here.
+from .models import Question
+
+
 def index(request):
-  return HttpResponse('Brrrrr')
+  latest_top = Question.objects.order_by('-publish_date')[:5]
+  
+  template = loader.get_template('polls/index.html')
+
+  context = {
+    'latest_top': latest_top
+  }
+
+  return HttpResponse(template.render(context, request))
+
+def detail(question, question_id):
+  return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s"
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s" % question_id)
